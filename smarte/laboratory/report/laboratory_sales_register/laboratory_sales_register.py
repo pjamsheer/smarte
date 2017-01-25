64a3c2ca376ca97fce136f00d9ccfd9ad90e5f21
Jamsheer <jamsheer@earthianslive.com>
2017-01-25 17:42:22 +0530

    Cutomer Patient change in sales registers

--- smarte/laboratory/report/laboratory_sales_register/laboratory_sales_register.py
+++ smarte/laboratory/report/laboratory_sales_register/laboratory_sales_register.py
@@ -20,12 +20,12 @@ def execute(filters=None):
 	invoice_income_map, invoice_tax_map = get_invoice_tax_map(invoice_list,
 		invoice_income_map, income_accounts)
 
-	
+
 	data = []
 	for inv in invoice_list:
 		# invoice details
-		
-		row = [inv.name, inv.posting_date, inv.customer,
+
+		row = [inv.name, inv.posting_date, inv.patient,
 		inv.debit_to]
 
 		# map income values
@@ -58,7 +58,7 @@ def get_columns(invoice_list):
 	"""return columns based on filters"""
 	columns = [
 		_("Invoice") + ":Link/Sales Invoice:120", _("Posting Date") + ":Date:80",
-		_("Customer") + ":Link/Customer:120",
+		_("Patient") + ":Link/Patient:120",
 		_("Receivable Account") + ":Link/Account:120"
 	]
 
@@ -90,7 +90,7 @@ def get_columns(invoice_list):
 def get_conditions(filters):
 	conditions = ""
 
-	if filters.get("customer"): conditions += "and customer = %(customer)s"
+	if filters.get("patient"): conditions += "and patient = %(patient)s"
 
 	if filters.get("from_date"): conditions += " and posting_date >= %(from_date)s"
 	if filters.get("to_date"): conditions += " and posting_date <= %(to_date)s"
@@ -99,10 +99,10 @@ def get_conditions(filters):
 
 def get_invoices(filters):
 	conditions = get_conditions(filters)
-	return frappe.db.sql("""select name, posting_date, debit_to, customer,
+	return frappe.db.sql("""select name, posting_date, debit_to, patient,
 		base_net_total, base_grand_total, base_rounded_total, outstanding_amount
 		from `tabSales Invoice`
-		where docstatus = 1 and billed_in = 'Laboratory' %s order by posting_date desc, name desc""" %
+		where docstatus = 1 %s order by posting_date desc, name desc""" %
 		conditions, filters, as_dict=1)
 
 def get_invoice_income_map(invoice_list):
@@ -135,4 +135,3 @@ def get_invoice_tax_map(invoice_list, invoice_income_map, income_accounts):
 			invoice_tax_map[d.parent][d.account_head] = flt(d.tax_amount)
 
 	return invoice_income_map, invoice_tax_map
-
