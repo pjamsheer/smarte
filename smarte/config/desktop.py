abd4c36943f2bd7ca3de49f8a75bbfc2ba747a2b
Jamsheer <jamsheer@earthianslive.com>
2017-02-01 16:05:53 +0530

    config desktop - for medical module

--- smarte/config/desktop.py
+++ smarte/config/desktop.py
@@ -5,31 +5,10 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Laboratory",
+			"module_name": "Medical",
 			"color": "#FF888B",
 			"icon": "octicon octicon octicon-beaker",
 			"type": "module",
-			"label": _("Laboratory")
-		},
-		{
-			"module_name": "OP",
-			"color": "#FF888B",
-			"icon": "octicon octicon octicon-plus",
-			"type": "module",
-			"label": _("OP")
-		},
-		{
-			"module_name": "IP",
-			"color": "#FF888B",
-			"icon": "octicon octicon octicon-pulse",
-			"type": "module",
-			"label": _("IP")
-		},
-		{
-			"module_name": "Pharmacy",
-			"color": "#FF888B",
-			"icon": "octicon octicon octicon-server",
-			"type": "module",
-			"label": _("Pharmacy")
+			"label": _("Medical")
 		}
 	]
