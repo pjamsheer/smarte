c1eafd633b0ae81a4c245c13054fcb0a7602391a
Jamsheer <jamsheer@earthianslive.com>
2017-02-06 11:39:49 +0530

    Add vital signs references to medical record

--- smarte/medical/doctype/vital_signs/vital_signs.py
+++ smarte/medical/doctype/vital_signs/vital_signs.py
@@ -7,10 +7,48 @@ import frappe
 from frappe.model.document import Document
 
 class VitalSigns(Document):
-	pass
+	def after_insert(self):
+		insert_vital_signs_to_medical_record(self)
+	def on_update(self):
+		update_vital_signs_to_medical_record(self)
 
 @frappe.whitelist()
 def create_vital_signs(patient):
 	vital_signs = frappe.new_doc("Vital Signs")
 	vital_signs.patient = patient
 	return vital_signs.as_dict()
+
+def insert_vital_signs_to_medical_record(doc):
+	subject = setting_subject_field(doc)
+	medical_record = frappe.new_doc("Patient Medical Record")
+	medical_record.patient = doc.patient
+	medical_record.subject = subject
+	medical_record.status = "Open"
+	medical_record.communication_date = doc.signs_date
+	medical_record.reference_doctype = "Vital Signs"
+	medical_record.reference_name = doc.name
+	medical_record.reference_owner = doc.owner
+	medical_record.save(ignore_permissions=True)
+
+def update_vital_signs_to_medical_record(doc):
+	medical_record_id = frappe.db.sql("select name from `tabPatient Medical Record` where reference_name=%s",(doc.name))
+	if(medical_record_id[0][0]):
+		subject = setting_subject_field(doc)
+		frappe.db.set_value("Patient Medical Record",medical_record_id[0][0],"subject",subject)
+
+def setting_subject_field(doc):
+	subject = "Vital Signs:: "
+	if(doc.temperature):
+		subject += "Pulse: \n"+ str(doc.temperature)+". "
+	if(doc.pulse):
+		subject += "Pulse: \n"+ str(doc.pulse)+". "
+	if(doc.respiratory_rate):
+		subject += "RR: \n"+ str(doc.respiratory_rate)+". "
+	if(doc.bp):
+		subject += "BP: \n"+ str(doc.bp)+". "
+	if(doc.bmi):
+		subject += "BMI: \n"+ str(doc.bmi)+". "
+	if(doc.nutrition_note):
+		subject += "Note: \n"+ str(doc.nutrition_note)+". "
+
+	return subject
