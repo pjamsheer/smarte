59b0e7bf106c275ff9f81df0c23dc9b8504863e5
Jamsheer <jamsheer@earthianslive.com>
2017-02-09 15:19:12 +0530

    Patient - Attachment to medical record

--- smarte/medical/doctype/patient/patient.py
+++ smarte/medical/doctype/patient/patient.py
@@ -7,12 +7,21 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils import cint, cstr
 from smarte.medical.doctype.op_settings.op_settings import generate_patient_id
+from smarte.medical.doctype.patient_medical_record.patient_medical_record import delete_attachment, insert_attachment
 class Patient(Document):
 	def after_insert(self):
 		generate_patient_id(self)
 
 	def autoname(self):
 		self.name = self.get_patient_name()
+
+	def on_update(self):
+		attachments = get_attachments(self)
+		if attachments:
+			delete_attachment("File", self.name)
+			for i in range(0,len(attachments)):
+				attachment = frappe.get_doc("File", attachments[i]['name'])
+				insert_attachment(attachment,attachment.attached_to_name)
 
 	def get_patient_name(self):
 		if frappe.db.get_value("Patient", self.patient_name):
@@ -22,3 +31,7 @@ class Patient(Document):
 			return "{0} - {1}".format(self.patient_name, cstr(count))
 
 		return self.patient_name
+
+def get_attachments(doc):
+	return frappe.get_all("File", fields=["name"],
+		filters = {"attached_to_name": doc.name, "attached_to_doctype": doc.doctype})
